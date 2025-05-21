7476a2a7034c7ab092aee0dabacb612352513410
Firstlink Consulting LLC <firstlinkconsultingllc@firstlinks-air.mynetworksettings.com>
2025-05-20 21:19:20 -0400

    Test push test

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .api.index import index
 from .api.movies import movies
 
+# Testing github commit
 app = Flask(__name__)
 app.register_blueprint(index)
 app.register_blueprint(movies)
